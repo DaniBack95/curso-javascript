2cb511934f2e559702323fe2a95c7ef336c23458
DaniBack95 <danielbaracaldoc.395@gmail.com>
2024-04-26 11:51:10 -0500

    AJAX: Asinchronous JavaSript y XML

---
+++ src/js/42_ajax.js
@@ -0,0 +1,18 @@
+/* 
+  AJAX:
+  Asinchronus JavaScript y XML
+  es una técnica de desarrollo web que permite la 
+  comunicación asíncrona entre una página web y un servidor. 
+  Esto significa que la página web puede actualizar 
+  parte de su contenido sin necesidad de recargar 
+  toda la página, lo que mejora la experiencia 
+  del usuario y la eficiencia.
+  Tenolgías que usa AJAX:
+    JavaScript
+    XMLHttpRequest(XHR)/Fetch
+    HTML
+    CSS
+    JSON/XML
+  AJAX permite crear aplicaciones web dinámicas y receptivas 
+  que pueden comunicarse de forma asíncrona con el servidor.
+*/
